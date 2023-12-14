fa5f61fcd278cbd407e69cc12b28f1b485d1118e
Hasbi Rizqulloh <hasbirzq08@gmail.com>
2023-12-14 17:53:20 +0700

    fikssssss

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize('dbs_pelma', 'faikar', 'dfasofd08', {
+const db = new Sequelize('dbs_pelma', 'faikar', 'dfasofd08git ', {
   host: 'faikar',
   dialect: 'mysql',
 });
