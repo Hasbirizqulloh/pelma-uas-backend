19c0a91550f6330381df5b1a3ea316b2e9d9a0eb
Hasbi Rizqulloh <hasbirzq08@gmail.com>
2023-12-14 20:16:22 +0700

    hagdhad

--- server.js
+++ server.js
@@ -37,7 +37,12 @@ app.use(
   })
 );
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.ORIGIN,
+  })
+);
 app.use(express.json());
 app.use(UserRouter);
 app.use(ReportRoute);
